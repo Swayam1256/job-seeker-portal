688a33c27801611030bd4a4c2618c9af3fbe4c60
Swayam1256 <swayamofficial37@gmail.com>
2026-05-21 20:29:00 +0530

    frontend deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://job-seeker-portal-ef92.onrender.com", 
+  withCredentials: true,
 });
 
 export default API;
